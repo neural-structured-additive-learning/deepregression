3653af2bd25eb6464a8650255a0b99d04b42cd50
davidruegamer <david.ruegamer@stat.uni-muenchen.de>
2021-10-25 17:56:46 +0200

    add back RWT to deepregression

--- R/helperfuns.R
+++ R/helperfuns.R
@@ -425,3 +425,40 @@ get_processor_name <- function(term)
   gsub("([^\\(])\\(.*","\\1", term)
   
 }
+
+###### TF Functions ######
+
+#' TensorFlow repeat function which is not available for TF 2.0
+#' 
+#' @param a tensor
+#' @param dim dimension for repeating
+#' 
+#' @export
+#' 
+tf_repeat <- function(a, dim)
+  tf$reshape(tf$tile(tf$expand_dims(a, axis = -1L),  c(1L, 1L, dim)), 
+             shape = list(-1L, a$shape[[2]]*dim))
+
+#' Row-wise tensor product using TensorFlow
+#' 
+#' @param a,b tensor
+#' 
+#' @export
+#' 
+tf_row_tensor <- function(a,b)
+{
+  tf$multiply(
+    tf_row_tensor_left_part(a,b),
+    tf_row_tensor_right_part(a,b)
+  )
+}
+
+tf_row_tensor_left_part <- function(a,b)
+{
+  tf_repeat(a, b$shape[[2]])
+}
+
+tf_row_tensor_right_part <- function(a,b)
+{
+  tf$tile(b, c(1L, a$shape[[2]]))
+}
