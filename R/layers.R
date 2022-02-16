1bc1aeefad294045c12cb64f368f7c2352071151
chriskolb <39267431+chriskolb@users.noreply.github.com>
2022-02-16 19:11:10 +0100

    implement group lasso regularizer

--- R/layers.R
+++ R/layers.R
@@ -16,6 +16,12 @@ inverse_group_lasso_pen = function(la) {
   layers$inverse_group_lasso_pen(la = la)
 } 
 
+regularizer_group_lasso = function(la, group_idx) {
+  python_path <- system.file("python", package = "deepregression")
+  layers <- reticulate::import_from_path("layers", path = python_path)
+  layers$ExplicitGroupLasso(la = la, group_idx = group_idx)
+}
+
 tibgroup_layer = function(units, group_idx, la, ...) {
   python_path <- system.file("python", package = "deepregression")
   layers <- reticulate::import_from_path("layers", path = python_path)
