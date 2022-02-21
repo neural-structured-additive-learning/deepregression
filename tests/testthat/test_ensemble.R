5fae35b4442423845a2a6a18a2e9482d96cfd4b0
lkook <lucasheinrich.kook@uzh.ch>
2022-02-21 09:28:27 +0100

    clean test

--- tests/testthat/test_ensemble.R
+++ tests/testthat/test_ensemble.R
@@ -3,23 +3,15 @@ context("Deep ensembles")
 test_that("deep ensemble", {
 
   n <- 1000
-  data = data.frame(matrix(rnorm(4*n), c(n,4)))
-  colnames(data) <- c("x1","x2","x3","xa")
-  formula <- ~ 1 + deep_model(x1,x2,x3) + s(xa) + x1
-
-  deep_model <- function(x) x %>%
-    layer_dense(units = 32, activation = "relu", use_bias = FALSE) %>%
-    layer_dropout(rate = 0.2) %>%
-    layer_dense(units = 8, activation = "relu") %>%
-    layer_dense(units = 1, activation = "linear")
+  data <- data.frame(matrix(rnorm(4*n), c(n,4)))
+  colnames(data) <- c("x1", "x2", "x3", "xa")
 
   y <- rnorm(n) + data$xa^2 + data$x1
 
   # check fake custom model
   mod <- deepregression(
     list_of_formulas = list(loc = ~ 1 + s(xa) + x1, scale = ~ 1),
-    data = data, y = y,
-    list_of_deep_models = list(deep_model = deep_model)
+    data = data, y = y
   )
 
   cf_init <- coef(mod)
