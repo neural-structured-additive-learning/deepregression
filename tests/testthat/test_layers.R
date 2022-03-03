e22558369cb9c32550e23f5832b5be77efd658fc
chriskolb <39267431+chriskolb@users.noreply.github.com>
2022-03-03 11:08:27 +0100

    Update test_layers.R

--- tests/testthat/test_layers.R
+++ tests/testthat/test_layers.R
@@ -3,23 +3,25 @@ context("deepregression layers")
 test_that("custom layers", {
   
   set.seed(42)
-  n <- 5000
-  x <- runif(n) %>% matrix(ncol=50)
-  y <- rnorm(n/50) + x[,1]
+  tf$random$set_seed(42)
+  n <- 1000
+  p <- 50
+  x <- runif(n*p) %>% matrix(ncol=p)
+  y <- 10*x[,1] + rnorm(n)
   
-  inp <- layer_input(shape=c(50), 
+  inp <- layer_input(shape=c(p), 
                      batch_shape = NULL)
-  out <- layer_hadamard_diff(units=1L, la=0.05, 
-                             initu = tf$initializers$constant(1e-3),
-                             initv = tf$initializers$constant(1e-3))(
+  out <- layer_hadamard_diff(units=1L, la=1, 
+                             initu = tf$initializers$constant(1e-6),
+                             initv = tf$initializers$constant(1e-6))(
                                inp
-                               )
+                             )
   mod <- keras_model(inp, out)
-    
-  mod %>% compile(loss="mse",
-                  optimizer = tf$optimizers$Adam())
   
-  mod %>% fit(x=x, y=matrix(y), epochs=400L, 
+  mod %>% compile(loss="mse",
+                  optimizer = tf$optimizers$Adam(learning_rate=1e-2))
+  
+  mod %>% fit(x=x, y=matrix(y), epochs=500L, 
               verbose=FALSE,
               validation_split=0.1)
   u <- as.matrix(mod$weights[[1]])
@@ -27,6 +29,8 @@ test_that("custom layers", {
   beta <- u^2-v^2
   expect_true(beta[1]>0.25)
   # does not work:
-  #expect_true(max(abs(beta[-1]))<1e-4)
-
+  expect_true(max(abs(beta[-1]))<1e-4)
+  
 })
+
+
