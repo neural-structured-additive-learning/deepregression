b2c35ff7de11d2c0154c0bb03466d4ce0d10d5f9
Christopher Marquardt <ch.marquardt@campus.lmu.de>
2023-07-29 18:10:06 +0200

    set to NULL to allow different subnetwork_builder,... for torch

--- R/deepregression.R
+++ R/deepregression.R
@@ -106,9 +106,9 @@ deepregression <- function(
   data,
   seed = as.integer(1991-5-4),
   return_prepoc = FALSE,
-  subnetwork_builder = subnetwork_init,
-  model_builder = keras_dr,
-  fitting_function = utils::getFromNamespace("fit.keras.engine.training.Model", "keras"),
+  subnetwork_builder = NULL,
+  model_builder = NULL,
+  fitting_function = NULL,
   additional_processors = list(),
   penalty_options = penalty_control(),
   orthog_options = orthog_control(),
@@ -121,11 +121,14 @@ deepregression <- function(
 )
 {
   
-  if(!is.null(seed) & engine == "tf")
-    try(tensorflow::set_random_seed(seed), silent = TRUE)
-  if(!is.null(seed)& engine == "torch")
-    try(torch::torch_manual_seed(seed), silent = TRUE)
-  
+  if(!is.null(seed)){
+    if(engine == "tf"){
+      try(tensorflow::set_random_seed(seed), silent = TRUE)
+    } else {
+      try(torch::torch_manual_seed(seed), silent = TRUE)
+      }
+    }
+
   # first check if an env is available
   if(!reticulate::py_available() & engine == "tf")
   {
@@ -139,13 +142,18 @@ deepregression <- function(
     message("Tensorflow not available. Use install_tensorflow().")
     invisible(return(NULL))
   }
-  # do the same as above for torch or encapsle it in function
   
-  if(engine == "torch"){
-    subnetwork_builder <- subnetwork_init_torch
-    model_builder <- torch_dr
-    fitting_function <- utils::getFromNamespace("fit.luz_module_generator",
-                                                "luz") 
+  if(engine == "tf"){
+    if(is.null(subnetwork_builder)) subnetwork_builder = subnetwork_init
+    if(is.null(model_builder)) model_builder = keras_dr
+    if(is.null(fitting_function)) fitting_function =
+        utils::getFromNamespace("fit.keras.engine.training.Model", "keras")
+  } else {
+    if(is.null(subnetwork_builder)) subnetwork_builder <- subnetwork_init_torch
+    if(is.null(model_builder)) model_builder <- torch_dr
+    if(is.null(fitting_function)) fitting_function <-
+        utils::getFromNamespace("fit.luz_module_generator",
+                                                "luz")
   }
   
   # convert data.frame to list
